22a2d39656d91c2d4a2b1c85a31f7cbeb58bdfb9
syahrul35 <irul.syahrulmaulana@gmail.com>
2025-12-12 14:38:52 +0700

    test CI/CD

--- resources/js/Pages/Setting/Index.jsx
+++ resources/js/Pages/Setting/Index.jsx
@@ -16,7 +16,7 @@ export default function Setting() {
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">
-                            You are in setting right now!
+                            You are in setting left now!
                         </div>
                     </div>
                 </div>
