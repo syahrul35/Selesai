ab40f08ee4c204e77db999c468996ecad787ebf2
syahrul35 <irul.syahrulmaulana@gmail.com>
2026-01-04 14:27:09 +0700

    fixing vite config for devops

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,24 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.jsx',
-            refresh: true,
-        }),
-        react(),
-    ],
-    server: {
-        host:'72.61.214.176',
-        https: false,
-        cors: false,
-        hmr: {
-            host: "72.61.214.176"
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        plugins: [
+            laravel({
+                input: 'resources/js/app.jsx',
+                refresh: true,
+            }),
+            react(),
+        ],
+        server: {
+            host: env.VITE_HOST || '0.0.0.0',
+            port: 5173,
+            hmr: {
+                host: env.VITE_HMR_HOST || 'localhost',
+            },
         },
-    },
+    };
 });
