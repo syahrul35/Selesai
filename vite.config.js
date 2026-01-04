e291d4a7b06334d0095554b2807e55017796c35b
syahrul35 <irul.syahrulmaulana@gmail.com>
2026-01-04 15:16:46 +0700

    fixing vite config js

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,16 @@
-import { defineConfig, loadEnv } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), '');
-
-    return {
-        plugins: [
-            laravel({
-                input: 'resources/js/app.jsx',
-                refresh: true,
-            }),
-            react(),
-        ],
-        server: {
-            host: env.VITE_HOST || '0.0.0.0',
-            port: 5173,
-            https: false,
-            cors: false,
-            hmr: {
-                host: env.VITE_HMR_HOST || 'localhost',
-            },
-        },
-    };
+export default defineConfig({
+    plugins: [
+        laravel({ input: "resources/js/app.jsx", refresh: true }),
+        react(),
+    ],
+    server: {
+        host: "72.61.214.176",
+        https: false,
+        cors: false,
+        hmr: { host: "72.61.214.176" },
+    },
 });
