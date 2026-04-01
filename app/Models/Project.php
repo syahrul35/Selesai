75030bf60e331a755d69ef722c308d3d18f750f4
syahrul35 <irul.syahrulmaulana@gmail.com>
2026-04-01 20:04:32 +0700

    update project and task model relational

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'name',
+    ];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -32,4 +32,9 @@ class Task extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
 }
