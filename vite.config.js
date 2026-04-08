c8540542a58fa2f79972cb359b8681375f70a683
syahrul35 <irul.syahrulmaulana@gmail.com>
2026-04-08 16:47:34 +0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig, loadEnv } from "vite";
 import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
+export default defineConfig( ({ mode }) => {
     const env = loadEnv(mode, process.cwd());
-    const isProduction = env.APP_ENV === "production";
+    const host = env.VITE_HOST || "localhost";
 
     return {
         plugins: [
