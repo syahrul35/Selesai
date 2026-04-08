3496e708ad7066b880a1d8a6432cd7da6f5220f4
syahrul35 <irul.syahrulmaulana@gmail.com>
2026-04-08 16:47:34 +0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,24 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-    plugins: [
-        laravel({ input: "resources/js/app.jsx", refresh: true }),
-        react(),
-    ],
-    // server: {
-    //     host: "https://selesai.syahrulmaulana.cloud",
-    //     https: true,
-    //     cors: false,
-    //     hmr: { host: "https://selesai.syahrulmaulana.cloud" },
-    // },
+export default defineConfig( ({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+    const host = env.VITE_HOST || "localhost";
+
+    return {
+        plugins: [
+            laravel({ input: "resources/js/app.jsx", refresh: true }),
+            react(),
+        ],
+        server: {
+            host,
+            https: env.VITE_HTTPS === "true",
+            cors: env.VITE_CORS,
+
+            hmr: {
+                host,
+            },
+        },
+    }
 });
