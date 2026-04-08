e3136d4b7dfb2a31688a13adf5f77f1187bc5d7b
syahrul35 <irul.syahrulmaulana@gmail.com>
2026-04-08 16:54:36 +0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig( ({ mode }) => {
+export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd());
     const host = env.VITE_HOST || "localhost";
 
@@ -27,4 +27,5 @@ export default defineConfig( ({ mode }) => {
                 cors: env.VITE_CORS,
             },
     };
+
 });
