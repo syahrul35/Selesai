3b6214c5303e40159ee852f028e24e164a6795c6
syahrul35 <irul.syahrulmaulana@gmail.com>
2026-04-08 16:47:34 +0700

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,29 @@ import { defineConfig, loadEnv } from "vite";
 import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig( ({ mode }) => {
+export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd());
-    const host = env.VITE_HOST || "localhost";
+    const isProduction = env.APP_ENV === "production";
 
     return {
         plugins: [
             laravel({ input: "resources/js/app.jsx", refresh: true }),
             react(),
         ],
-        server: {
-            host,
-            https: env.VITE_HTTPS === "true",
-            cors: env.VITE_CORS === "true",
 
-            hmr: {
-                host,
-            },
-        },
-    }
+        server: isProduction
+            ? {
+                  host: "env.VITE_HOST",
+                  https: env.VITE_HTTPS === "true",
+                  cors: env.VITE_CORS === "true",
+                  hmr: {
+                      host: "env.VITE_HOST",
+                  },
+              }
+            : {
+                  host: env.VITE_HOST || "localhost",
+                  https: env.VITE_HTTPS === "true",
+                  cors: env.VITE_CORS,
+              },
+    };
 });
